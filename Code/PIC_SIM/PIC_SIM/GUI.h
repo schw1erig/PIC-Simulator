cb5eeb93c5d2b2963bd19dc9a92e2c4bce10fad1
MrMarcelA380 <marcel.fischer@swr.de>
2023-04-17 15:53:47 +0200

    Main-Projekt + Beispiel erstellt

---
+++ Code/PIC_SIM/PIC_SIM/GUI.h
@@ -0,0 +1,16 @@
+#pragma once
+
+//#include "befehle.h"
+//#include "Form1.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+void but1_click(TextBox^ textBox1, TextBox^ textBox2, TextBox^ textBox3 ) {
+	int x = Convert::ToInt32(textBox1->Text);
+	int y = Convert::ToInt32(textBox2->Text);
+
+	int erg = add(x, y);
+
+	textBox3->AppendText(String::Format("Summe={0}\n", erg));
+}
---
+++ Code/PIC_SIM/PIC_SIM/befehle.h
@@ -0,0 +1,7 @@
+#pragma once
+
+int add(int x, int y) {
+
+	return x+y;
+
+}
