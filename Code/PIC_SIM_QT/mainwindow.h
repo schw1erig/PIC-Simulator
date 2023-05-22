500ea198e3226d1af8ae39658a56185b997b595c
Misch <Christian.Misch@its-technidata.de>
2023-05-22 16:14:47 +0200

    Funktionen zur Änderungen von Labeln

--- Code/PIC_SIM_QT/mainwindow.h
+++ Code/PIC_SIM_QT/mainwindow.h
@@ -16,19 +16,84 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_toolButton_toggled(bool checked);
-
-    void on_pushButton_5_toggled(bool checked);
-
-    void on_test_clicked(bool checked);
-
-    void on_test_stateChanged(int arg1);
-
-    void on_pushButton_5_clicked();
-
-    void on_test_clicked();
 
     void on_Console_Field_blockCountChanged(int newBlockCount);
+
+
+    QString to_QString(int);
+
+    //Box:Konsole
+    void gui_set_Console_field();
+
+    //Box:Register
+    void gui_set_dataSpeicher_table(int row, int column);
+    void gui_get_dataSpeicher_table();
+
+    //Box:Pins
+    void gui_set_pin_table();
+
+    //Box:Quarzfrequenz
+    void gui_set_quarzfrequenz_Label(int quarztakt);
+
+    //Box:Laufzeit
+    void gui_set_laufzeit_Label();
+
+    //Box:sichtbar
+    void gui_set_wReg_Label();
+    void gui_set_fsr_Label();
+    void gui_set_pcl_Label();
+    void gui_set_pcLath_Label();
+    void gui_set_status_Label();
+
+    //Box:unsichtbar
+    void gui_set_pc_Label();
+    void gui_set_stackpointer_Label();
+    void gui_set_vt_Label();
+    void gui_set_wdt_Label();
+    bool gui_check_wdt_aktiv();
+
+    //Box 1 unten links
+    void gui_set_irp_Label();
+    void gui_set_rp1_Label();
+    void gui_set_rp0_Label();
+    void gui_set_t0_Label();
+    void gui_set_pd_Label();
+    void gui_set_z_Label();
+    void gui_set_dc_Label();
+    void gui_set_c_Label();
+
+    void gui_set_option_Label();
+
+    // Box 2 unten links
+    void gui_set_rbp_Label();
+    void gui_set_intedg_Label();
+    void gui_set_t0cs_Label();
+    void gui_set_t0se_Label();
+    void gui_set_psa_Label();
+    void gui_set_ps2_Label();
+    void gui_set_ps1_Label();
+    void gui_set_ps0_Label();
+
+    void gui_set_intcon_Label();
+    
+
+    // Box 3 unten links
+    void gui_set_gie_Label();
+    void gui_set_pie_Label();
+    void gui_set_t0ie_Label();
+    void gui_set_inte_Label();
+    void gui_set_rbie_Label();
+    void gui_set_t0if_Label();
+    void gui_set_intf_Label();
+    void gui_set_rbif_Label();
+
+
+
+
+
+
+
+
 
 private:
     Ui::MainWindow *ui;
